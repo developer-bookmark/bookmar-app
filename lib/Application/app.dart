9b55fd54ee214a860ee10dce20e997574ea33b08
Ivan Putra Eriansya <eriansha.van@gmail.com>
2024-07-24 18:59:58 +0700

    ref: split main and app

--- lib/Application/app.dart
+++ lib/Application/app.dart
@@ -1,12 +1,8 @@
+import 'package:bookmarking_app/Features/Common/Views/homepage.dart';
 import 'package:flutter/cupertino.dart';
-import '../Features/Common/Views/homepage.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BookmarkApp extends StatelessWidget {
+  const BookmarkApp({super.key});
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:bookmarking_app/Application/app.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const BookmarkApp());
+}
